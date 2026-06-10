9a028a203e8f78d7907956e654337b771304a9a0
Jyothi <bhupathiraju0099@gmail.com>
2026-06-10 16:13:59 +0530

    initial calculator script

---
+++ calculator-project/calculator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Author:Bhupathiraju Jyothi
+#Date:10/06/2025
+#Performing calculator example
+
+echo "calculator"
+echo "enter first number"
+read a
+echo "enter second number"
+read b
+sum=$((a+b))
+echo "$sum"
+
